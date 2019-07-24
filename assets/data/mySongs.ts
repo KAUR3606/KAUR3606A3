25753d60e5e5cfbf5c102f3b900b18bec4cdbecf
MANVIR KAUR <52471652+KAUR3606@users.noreply.github.com>
2019-07-23 23:47:28 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,33 @@
+import { Favourites } from '../../app/favSongs';
+
+export const Myfavourites: Favourites[]= [
+    {
+        songName: "Don't let me Down",
+        artist: 'The ChainSmokers',
+        gener: 'Trap Music, Dance/Electronic, Pop',
+        picture: './assets/images/Song1.png',
+        yearReleased: 2016
+    },
+    {
+        songName: "To Be Human",
+        artist: 'Sia',
+        gener: 'Pop',
+        picture: './assets/images/Song2.jpg',
+        yearReleased: 2017
+    },
+    {
+        songName: "Something Just Like This",
+        artist: 'Coldplay, The Chainsmokers',
+        gener: 'Dance/Electronic',
+        picture: './assets/images/Song3.png',
+        yearReleased: 2017
+    },
+    {
+        songName: "Love story",
+        artist: 'Taylor Swift',
+        gener: 'Country Music, Country, Country Pop, Pop',
+        picture: './assets/images/Song4.png',
+        yearReleased: 2009
+    }
+
+    ]
